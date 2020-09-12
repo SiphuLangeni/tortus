e27f626378515ef49dab749b98fff99fda09c007
Siphu Langeni, MS <szlangeni@gmail.com>
2020-09-12 14:19:39 -0400

    Include constructor documentation

--- docs/conf.py
+++ docs/conf.py
@@ -33,6 +33,7 @@ extensions = [
     'sphinx.ext.coverage'
 ]
 
+autoclass_content = 'both'
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 

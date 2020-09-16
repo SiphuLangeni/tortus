75aa44327bd12635a6838a38842ca72270378930
Siphu Langeni, MS <szlangeni@gmail.com>
2020-09-16 15:19:20 -0400

    Add find_packages

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
@@ -11,6 +11,8 @@ setup(
     author='Siphu Langeni',
     author_email='szlangeni@gmail.com',
     py_modules=['tortus'],
+    packages=find_packages(include=['src']),
+    include_package_data=True,
     package_dir={'': 'src'},
     classifiers=[
         'Programming Language :: Python :: 3.6',
@@ -20,8 +22,12 @@ setup(
         'Operating System :: OS Independent'
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
     ],
+    keywords=[
+        'nlp', 'annotation', 'labeling', 'jupyter-notebook', 'ipywidgets'
+    ],
     long_description=long_description,
     long_description_content_type='text/markdown',
+    python_requires='>=3.6',
     install_requires=[
         'pandas', 'ipywidgets', 'ipython', 'notebook', 'jupyter_contrib_nbextensions'
     ],
